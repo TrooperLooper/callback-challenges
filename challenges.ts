294280fb25a9ddf5c90458d7a63c5271d06b51a0
Lars <lars@recap.dk>
2025-09-04 13:27:26 +0200

    Initial commit

---
+++ challenges.ts
@@ -0,0 +1,29 @@
+// Challenge 1
+/*  Create a function addTwo that accepts one input and adds 2 to it. */
+
+// To check if you've completed it, uncomment these console.logs!
+// console.log(addTwo(3));
+// console.log(addTwo(10));
+
+// ________________________________________________________________________________________________
+// Challenge 2
+/* 
+Create a function addS that accepts one input and adds an "s" to it.
+*/
+
+const add =()
+
+// uncomment these to check your work
+//console.log(addS("pizza"));
+//console.log(addS("bagel"));
+
+// ________________________________________________________________________________________________
+// Challenge 3
+/* 
+Create a function called map that takes two inputs:
+1. An array of numbers (a list of numbers)
+2. A 'callback' function - a function that is applied to each element of the array (inside of the function 'map')
+Have map return a new array filled with numbers that are the result of using the 'callback' function on each element of the input array.
+*/
+//console.log(map([1, 2, 3], addTwo));
+
